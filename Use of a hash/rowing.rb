9f74437b202cc06bc0b0bda07d1cb42b394366db
Kynan <k.song@blueyonder.co.uk>
2017-10-10 09:14:57 +0100

    Added rowing.rb for hashes.

---
+++ Use of a hash/rowing.rb
@@ -0,0 +1,22 @@
+types_of_boat = {
+  single_scull: {
+    oars: 2,
+    crew: 1
+  },
+  eight: {
+    oars: 8,
+    crew: 9
+  },
+  four: {
+    oars: 4,
+    crew: 5
+  },
+  double: {
+    oars: 4,
+    crew: 2
+  },
+  pair:{
+    oars: 2,
+    crew: 2
+  }
+}
