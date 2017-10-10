8b69c6c9180f1bebf31145d207a5923e90678e8b
Kynan <k.song@blueyonder.co.uk>
2017-10-10 10:05:14 +0100

    Function can call a boat and return a crew.

--- Use of hashes/rowing.rb
+++ Use of hashes/rowing.rb
@@ -1,4 +1,4 @@
-types_of_boat = {
+@types_of_boat = {
   single_scull: {
     oars: 2,
     crew: 1
@@ -21,4 +21,14 @@ types_of_boat = {
   }
 }
 
-puts types_of_boat[:four][:crew]
+puts @types_of_boat[:four][:crew]
+
+def find_crew(boat_type)
+  for boat, name in @types_of_boat
+    if boat == boat_type
+      return name[:crew]
+    end
+  end
+end
+
+puts find_crew(:eight)
